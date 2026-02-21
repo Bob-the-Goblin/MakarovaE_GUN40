91fbb1f957744b6e7e0ce9c01441aeb8f9e7b87a
Bob <lizokdoughnut03@gmail.ru>
2026-02-21 14:27:35 +0300

    addedZenject

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void OpenMainScene()
+    { 
+        SceneManager.LoadScene(0); 
+    }
+
+    public void OpenGameScene()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+
+
+}

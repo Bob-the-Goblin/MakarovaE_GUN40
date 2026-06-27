78dee5828c1f37ea5ab42f829b41da69fa7510b1
Bob <lizokdoughnut03@gmail.ru>
2026-06-27 18:44:06 +0300

    started

---
+++ Assets/Scripts/RobotCleaner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotCleaner : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    Ray ray = new Ray();
+
+    private void CheackEnviropment()
+    { }
+
+    private void FindRandomWay()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
